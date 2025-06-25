f5122b84c4c8ac426de692e0b2f159b24fb708d1
wilmagonzalezzumbardo <91170656+wilmagonzalezzumbardo@users.noreply.github.com>
2025-06-25 23:37:29 +0000

    Correccion del archivo api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,8 +6,7 @@
     use App\Http\Controllers\PonenteController;
     use App\Http\Controllers\AsistenteController;
 
-    Route::apiResource('ponentes', PonenteController::class);
-    Route::apiResource('asistentes', AsistenteController::class);
+   
 
 /*  rutas publicas */
 // Recuperar todos los eventos
@@ -25,28 +24,27 @@ Route::get('/ponentes/{id}', [PonenteController::class, 'show']);
 */
 Route::middleware('auth:api')->group(function () 
 {
-    // Almacenar un evento nuevo
-    Route::post('/eventos', [EventoController::class, 'store']);
-    // Actualizar un evento específico
-    Route::put('/eventos/{evento}', [EventoController::class, 'update']);
-    // Eliminar un evento específico
-    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
-    // Almacenar un ponente nuevo
-    Route::post('/ponentes', [PonenteController::class, 'store']);
-    // Actualizar un ponente específico
-    Route::put('/ponentes/{ponente}', [PonenteController::class, 'update']);
-    // Eliminar un ponente específico
-    Route::delete('/ponentes/{id}', [PonenteController::class, 'destroy']);
-    // Recuperar todos los asistentes
-    Route::get('/asistentes', [AsistenteController::class, 'index']);
-    // Almacenar un asistente nuevo
-    Route::post('/asistentes', [AsistenteController::class, 'store']);
-    // Recuperar un asistente específico
-    Route::get('/asistentes/{id}', [AsistenteController::class, 'show']);
-    // Actualizar un asistente específico
-    Route::put('/asistentes/{asistente}', [AsistenteController::class,'update']);
-    // Eliminar un asistente específico
-    Route::delete('/asistentes/{id}', [AsistenteController::class,'destroy']);
-    Route::get('/asistentes', [PonenteController::class, 'index']);
-    Route::get('/asistentes/{id}', [PonenteController::class, 'show']);
+    
+    Route::post('/eventos', [EventoController::class, 'store']); // Almacenar un evento nuevo
+    
+    Route::put('/eventos/{evento}', [EventoController::class, 'update']); // Actualizar un evento específico
+    
+    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']); // Eliminar un evento específico
+    
+    Route::post('/ponentes', [PonenteController::class, 'store']); // Almacenar un ponente nuevo
+    
+    Route::put('/ponentes/{ponente}', [PonenteController::class, 'update']); // Actualizar un ponente específico
+    
+    Route::delete('/ponentes/{id}', [PonenteController::class, 'destroy']); // Eliminar un ponente específico
+    
+    Route::get('/asistentes', [AsistenteController::class, 'index']); // Recuperar todos los asistentes
+    
+    Route::post('/asistentes', [AsistenteController::class, 'store']); // Almacenar un asistente nuevo
+    
+    Route::get('/asistentes/{id}', [AsistenteController::class, 'show']); // Recuperar un asistente específico
+    
+    Route::put('/asistentes/{asistente}', [AsistenteController::class,'update']); // Actualizar un asistente específico
+     
+    Route::delete('/asistentes/{id}', [AsistenteController::class,'destroy']); // Eliminar un asistente específico
+     
 });
